107bd929797ff131b7892e72c677910d665d6de2
srimantika <srimantika@gmail.com>
2021-11-25 21:53:10 +0000

    Code for getting all reservations from database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -53,7 +53,7 @@ exports.getUserWithEmail = getUserWithEmail;
 
 const getUserWithId = function(id) {
   return pool.query(`SELECT * FROM users where id = $1`, [id])
-  .then((result) => {console.log(result.rows)
+  .then((result) => {
     return result.rows[0] 
    })
   .catch((err) => {
@@ -101,9 +101,33 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
+/*const getAllReservations = function(guest_id, limit = 10) {
   return getAllProperties(null, 2);
+}*/
+const getAllReservations = function(guest_id, limit = 10) {
+  const queryString = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+  return pool.query(queryString, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return console.log(err.message);
+    });
 }
+
+
+
 exports.getAllReservations = getAllReservations;
 
 /// Properties
